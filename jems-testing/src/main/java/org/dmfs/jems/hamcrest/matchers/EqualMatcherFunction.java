540d5c3a58226b0ef8c1f1b8a40a640c1138a87a
Gabor Keszthelyi <gabor.keszthelyi@gmail.com>
2017-10-18 14:10:22 +0200

    Functions to convert T to Matcher<T> for same instance and object equality. #48

---
+++ jems-testing/src/main/java/org/dmfs/jems/hamcrest/matchers/EqualMatcherFunction.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2017 dmfs GmbH
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dmfs.jems.hamcrest.matchers;
+
+import org.dmfs.jems.function.Function;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.Matcher;
+
+
+/**
+ * {@link Function} that creates a {@link Matcher} for the given argument that matches for object equality (equals()).
+ *
+ * @author Gabor Keszthelyi
+ */
+public final class EqualMatcherFunction<T> implements Function<T, Matcher<T>>
+{
+    private static final Function INSTANCE = new EqualMatcherFunction<>();
+
+
+    public static <T> Function<T, Matcher<T>> instance()
+    {
+        //noinspection unchecked
+        return (Function<T, Matcher<T>>) INSTANCE;
+    }
+
+
+    @Override
+    public Matcher<T> value(T target)
+    {
+        return CoreMatchers.equalTo(target);
+    }
+}
