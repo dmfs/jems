c3db338e3a2c2df475037f389f51e5c25ba86b75
Gabor Keszthelyi <gabor.keszthelyi@gmail.com>
2017-12-11 16:20:27 +0100

    File templates for class, header, junit4 class. #125 (#126)

---
+++ .idea/fileTemplates/code/JUnit4 Test Class.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+import ${CLASS_NAME};
+
+
+/**
+ * Test for {@link ${CLASS_NAME}}.
+ *
+ * @author ${USER}
+ */
+public final class ${NAME} {
+
+  @Test
+  public void test()
+  {
+      assertThat(new ${CLASS_NAME}(), );
+  }
+  
+  ${BODY}
+}
