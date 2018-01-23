209a224cab7ce209307d93b2eab45144d705e01a
Gabor Keszthelyi <gabor.keszthelyi@gmail.com>
2018-01-23 15:15:01 +0100

    MapFlattened Optional decorator for the flatmap operation. #145

---
+++ src/main/java/org/dmfs/jems/optional/decorators/MapFlattened.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2018 dmfs GmbH
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dmfs.jems.optional.decorators;
+
+import org.dmfs.jems.function.Function;
+import org.dmfs.jems.optional.adapters.Collapsed;
+import org.dmfs.optional.Optional;
+import org.dmfs.optional.decorators.DelegatingOptional;
+
+
+/**
+ * {@link Optional} decorator corresponding to the flatmap operation.
+ * <p>
+ * It takes a mapping {@link Function} which returns an {@link Optional} which serves as the 'delegate' value if the original value is present.
+ * <p>
+ * (Flattening here means turning {@code Optional<Optional<T>} into {@code Optional<T>}.)
+ *
+ * @author Gabor Keszthelyi
+ */
+public final class MapFlattened<From, To> extends DelegatingOptional<To>
+{
+    public MapFlattened(Function<From, Optional<To>> mapFunction, Optional<From> original)
+    {
+        super(new Collapsed<>(new Mapped<>(mapFunction, original)));
+    }
+
+}
---
+++ src/test/java/org/dmfs/jems/optional/decorators/MapFlattenedTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2018 dmfs GmbH
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dmfs.jems.optional.decorators;
+
+import org.dmfs.optional.Present;
+import org.junit.Test;
+
+import static org.dmfs.jems.hamcrest.matchers.AbsentMatcher.isAbsent;
+import static org.dmfs.jems.hamcrest.matchers.PresentMatcher.isPresent;
+import static org.dmfs.optional.Absent.absent;
+import static org.junit.Assert.assertThat;
+
+
+/**
+ * Test for {@link MapFlattened}.
+ *
+ * @author Gabor Keszthelyi
+ */
+public final class MapFlattenedTest
+{
+
+    @Test
+    public void test()
+    {
+        assertThat(new MapFlattened<>(i -> new Present<>(String.valueOf(i)), new Present<>(3)), isPresent("3"));
+
+        assertThat(new MapFlattened<>(i -> new Present<>(String.valueOf(i)), absent()), isAbsent());
+
+        assertThat(new MapFlattened<>(i -> absent(), new Present<>(3)), isAbsent());
+
+        assertThat(new MapFlattened<>(i -> absent(), absent()), isAbsent());
+    }
+
+}
