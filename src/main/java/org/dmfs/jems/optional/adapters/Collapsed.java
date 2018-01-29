e6e302f79559d6b3e5d344569811a078b7741139
Gabor Keszthelyi <gabor.keszthelyi@gmail.com>
2018-01-29 21:41:54 +0100

    MapFlattened Optional decorator for the flatmap operation. #145 (#146)

---
+++ src/main/java/org/dmfs/jems/optional/adapters/Collapsed.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2018 dmfs GmbH
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dmfs.jems.optional.adapters;
+
+import org.dmfs.optional.Optional;
+
+import java.util.NoSuchElementException;
+
+import static org.dmfs.optional.Absent.absent;
+
+
+/**
+ * Collapses the given {@link Optional} of {@link Optional} into {@link Optional}.
+ *
+ * @author Marten Gajda
+ * @author Gabor Keszthelyi
+ */
+public final class Collapsed<T> implements Optional<T>
+{
+    private final Optional<Optional<T>> mDelegate;
+
+
+    public Collapsed(Optional<Optional<T>> delegate)
+    {
+        mDelegate = delegate;
+    }
+
+
+    @Override
+    public boolean isPresent()
+    {
+        return mDelegate.value(absent()).isPresent();
+    }
+
+
+    @Override
+    public T value(T defaultValue)
+    {
+        return mDelegate.value(absent()).value(defaultValue);
+    }
+
+
+    @Override
+    public T value() throws NoSuchElementException
+    {
+        return mDelegate.value().value();
+    }
+}
---
+++ src/main/java/org/dmfs/jems/optional/decorators/MapCollapsed.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2018 dmfs GmbH
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dmfs.jems.optional.decorators;
+
+import org.dmfs.jems.function.Function;
+import org.dmfs.jems.optional.adapters.Collapsed;
+import org.dmfs.optional.Optional;
+import org.dmfs.optional.decorators.DelegatingOptional;
+
+
+/**
+ * {@link Optional} decorator maps the given {@link Optional} to an {@link Optional} and collapses the result.
+ *
+ * @author Gabor Keszthelyi
+ */
+public final class MapCollapsed<From, To> extends DelegatingOptional<To>
+{
+    public MapCollapsed(Function<From, Optional<To>> mapFunction, Optional<From> original)
+    {
+        super(new Collapsed<>(new Mapped<>(mapFunction, original)));
+    }
+
+}
