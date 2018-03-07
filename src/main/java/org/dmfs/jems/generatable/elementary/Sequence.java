a862b5ab1a21e5adc89279854d25158974c2ba8b
Marten Gajda <marten@dmfs.org>
2018-03-07 01:55:35 +0100

    Add new Generator and Generatable types. Implements #152 (#154)
    This commit adds two new types to implement something like Iterable/Iterator for generated, infinite series. It also adds a Matcher which tests the start sequence of Generabtables.
    In addition it adds initial implementations for Sequences which are based on an initial value and a Function which calculates the next value based on that.
    
    In order to use Generatables like Iterables (which needs to be done carefully because they are infinite) there are initial Generatable-Iterable and Generator-Iterator adapters.

---
+++ src/main/java/org/dmfs/jems/generatable/elementary/Sequence.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2018 dmfs GmbH
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dmfs.jems.generatable.elementary;
+
+import org.dmfs.jems.function.Function;
+import org.dmfs.jems.generatable.Generatable;
+import org.dmfs.jems.generator.Generator;
+
+
+/**
+ * A sequence {@link Generatable}. It generates a sequence by passing the previous result (or initial value) into a given {@link Function}.
+ *
+ * @author Marten Gajda
+ */
+public final class Sequence<T> implements Generatable<T>
+{
+    private T mFirst;
+    private final Function<T, T> mFunction;
+
+
+    public Sequence(T first, Function<T, T> function)
+    {
+        mFirst = first;
+        mFunction = function;
+    }
+
+
+    @Override
+    public Generator<T> generator()
+    {
+        return new org.dmfs.jems.generator.elementary.Sequence<>(mFirst, mFunction);
+    }
+}
---
+++ src/main/java/org/dmfs/jems/generator/elementary/Sequence.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2018 dmfs GmbH
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dmfs.jems.generator.elementary;
+
+import org.dmfs.jems.function.Function;
+import org.dmfs.jems.generator.Generator;
+
+
+/**
+ * A sequence {@link Generator}. It generates a sequence by passing the previous result (or initial value) into a given {@link Function}.
+ *
+ * @author Marten Gajda
+ */
+public final class Sequence<T> implements Generator<T>
+{
+    private T mNext;
+    private Function<T, T> mFunction;
+
+
+    public Sequence(T first, Function<T, T> function)
+    {
+        mNext = first;
+        mFunction = value -> {
+            // the first call returns value as is, afterwards we use the given function
+            mFunction = function;
+            return value;
+        };
+    }
+
+
+    @Override
+    public T next()
+    {
+        return mNext = mFunction.value(mNext);
+    }
+}
