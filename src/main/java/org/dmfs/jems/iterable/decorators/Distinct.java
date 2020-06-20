56e4a4a1e50a6c556a82bde00133e968b7b50d28
Marten Gajda <marten@dmfs.org>
2020-06-20 10:27:09 +0200

    New Distinct Decorators, implements #275

---
+++ src/main/java/org/dmfs/jems/iterable/decorators/Distinct.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2020 dmfs GmbH
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dmfs.jems.iterable.decorators;
+
+import org.dmfs.iterables.decorators.DelegatingIterable;
+import org.dmfs.jems.function.Function;
+
+import java.util.Iterator;
+
+
+/**
+ * An {@link Iterator} which drops duplicates of already iterated elements.
+ * <p>
+ * Examples
+ *
+ * <pre>
+ * Distinct([1, 2, 2, 1, 2, 3, 3]) ->
+ * [1, 2, 3]
+ *
+ * Distinct(String::length, ["abc", "xyz", "42", "21"]) ->
+ * ["abc", "42"]
+ * </pre>
+ */
+public final class Distinct<T> extends DelegatingIterable<T>
+{
+    public <V> Distinct(Iterable<T> delegate)
+    {
+        super(() -> new org.dmfs.jems.iterator.decorators.Distinct<>(delegate.iterator()));
+    }
+
+
+    public <V> Distinct(Function<? super T, ? extends V> criterionFunction, Iterable<T> delegate)
+    {
+        super(() -> new org.dmfs.jems.iterator.decorators.Distinct<>(criterionFunction, delegate.iterator()));
+    }
+}
