015546499529951e4e0dfa4647cb38f3eda8ad12
Marten Gajda <marten@dmfs.org>
2020-07-09 14:57:17 +0200

    Fix Javadoc, fixes #281 (#284)

--- src/main/java/org/dmfs/jems/iterator/decorators/Distinct.java
+++ src/main/java/org/dmfs/jems/iterator/decorators/Distinct.java
@@ -33,10 +33,10 @@ import java.util.Set;
  * Examples
  *
  * <pre>
- * Distinct([1, 2, 2, 1, 2, 3, 3]) ->
+ * Distinct([1, 2, 2, 1, 2, 3, 3]) -&gt;
  * [1, 2, 3]
  *
- * Distinct(String::length, ["abc", "xyz", "42", "21"]) ->
+ * Distinct(String::length, ["abc", "xyz", "42", "21"]) -&gt;
  * ["abc", "42"]
  * </pre>
  */
